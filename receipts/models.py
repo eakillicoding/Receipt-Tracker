9f1e69dfd418240b9d2ab40fccba13a835865a30
Emre <eakillicoding@gmail.com>
2023-07-19 15:28:24 -0400

    Feature 5 complete

--- receipts/models.py
+++ receipts/models.py
@@ -45,5 +45,8 @@ class Receipt(models.Model):
         on_delete=models.CASCADE,
     )
     account = models.ForeignKey(
-        "Account", related_name="receipts", on_delete=models.CASCADE, null=True
+        "Account",
+        related_name="receipts",
+        on_delete=models.CASCADE,
+        null=True,
     )
--- receipts/views.py
+++ receipts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from receipts.models import Receipt
 
 # Create your views here.
+
+
+def receipt_list(request):
+    receipts = Receipt.objects.filter(purchaser=request.user)
+    context = {
+        "receipts": receipts,
+    }
+    return render(request, "receipts/list.html", context)
