298fcecc3a4ba53dce987f5cef5e484496e2fee8
Emre <eakillicoding@gmail.com>
2023-07-19 21:32:13 -0400

    Feature 12 complete

--- receipts/urls.py
+++ receipts/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from receipts.views import (
     receipt_list,
     create_receipt,
+    category_list,
+    account_list,
 )
 
 urlpatterns = [
     path("", receipt_list, name="home"),
     path("create/", create_receipt, name="create_receipt"),
+    path("categories/", category_list, name="category_list"),
+    path("accounts/", account_list, name="account_list"),
 ]
