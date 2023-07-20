5a1262b8aa5c2704d094db5e65ec6c459d6c771d
Emre <eakillicoding@gmail.com>
2023-07-20 00:58:01 -0400

    Feature 14 complete

--- receipts/forms.py
+++ receipts/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from receipts.models import (
     Receipt,
     ExpenseCategory,
+    Account,
 )
 
 
@@ -22,3 +23,12 @@ class ExpenseCategoryForm(forms.ModelForm):
     class Meta:
         model = ExpenseCategory
         fields = ("name",)
+
+
+class AccountForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = (
+            "name",
+            "number",
+        )
